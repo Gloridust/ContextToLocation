2b60002d46a0f246e1f297a153b412b87d51db59
Gloridust <Gloridust@gmail.com>
2024-07-14 18:39:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 model_name = 'gemma2' 
 
 pre_prompt1='''
-你是一个旅游大数据规划专家，从下面出行计划文本中，规划出旅客对应每个景点的到达时间。如果遇到不确定的数据，通过你的经验推测。以到达地点-时间点的格式，直接输出你的推测结果，其中时间是指你推测的时刻，如：14:00。
+你是一个旅游大数据规划专家，从下面出行计划文本中，规划出自驾旅客对应每个景点的到达时间。如果遇到不确定的数据，通过你的经验推测。以到达地点-时间点的格式，直接输出你的推测结果，其中时间是指你推测的时刻，如：14:00。
 '''
 
 pre_prompt2='''
@@ -71,7 +71,7 @@ def process_data_file(file_path):
             f.write('\n')
             
 def main():
-    file_path = 'your_file.xlsx'
+    file_path = './data/wb-chengdu-city.csv'
     process_data_file(file_path)
 
 if __name__=="__main__":
