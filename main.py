029b2fa25f94cc5ed019100e40fd3bbda4841dc1
Gloridust <Gloridust@gmail.com>
2024-07-15 10:01:02 +0800

    add: pre_prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ollama
+from pre_prompt import pre_prompt1,pre_prompt2
 import pandas as pd
 import json
 import os
@@ -6,32 +7,11 @@ import glob
 from concurrent.futures import ThreadPoolExecutor
 import logging
 
+#####config#####
 model_name = 'gemma2' 
 description_col='description'
 max_workers=8
-
-pre_prompt1='''
-你是一个旅游大数据规划专家，从下面出行计划文本中，规划出自驾旅客对应每个景点的到达时间。如果遇到不确定的数据，通过你的经验推测出来。以到达地点-时间点的格式，直接输出你的推测结果，其中时间是指你推测的时刻，如：14:00。
-'''
-
-pre_prompt2='''
-You are a data processing robot. Your function is to extract the exact time and location of a person's arrival at a certain location and output it in json format.
-Be sure to figure out the exact time of day, such as 12:00, not other text, which must be in the format of HH:MM;
-You need to consider that this time must be reasonable. Based on your experience, what time of day he might visit there.
-Output ONLY valid JSON array, nothing else. Example format:
-[
-  {
-    "location": "Chengdu People's Park",
-    "time": "12:00"
-  },
-  {
-    "location": "Chengdu Museum",
-    "time": "14:00"
-  }
-]
-
-Process the following information:
-'''
+################
 
 # 设置日志
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
